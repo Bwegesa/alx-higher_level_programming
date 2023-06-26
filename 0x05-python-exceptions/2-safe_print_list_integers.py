d112cc6f051062ce2db9a5c2993cf97023a313a9
Bwegesa <wegesamaroa.7@gmail.com>
2023-06-26 13:13:55 -0700

    Write a function that prints the first x elements of a list and only

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             real_no_of_elems += 1
         except(ValueError, TypeError):
             pass
-        print()
-        return real_no_of_elems
+    print()
+    return real_no_of_elems
